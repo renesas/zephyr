silverlee35/z_example

drivers: flash: update flash driver source for Renesas RA
- This update is to change to use irq_lock instead of semaphore
for code flash

- Modify and add conditions to check and make decision to perform
action at last block.

Signed-off-by: Khoa Nguyen <khoa.nguyen.xh@renesas.com>
Signed-off-by: Tran Van Quy <quy.tran.pz@renesas.com>

drivers/flash/flash_hp_ra.c
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <stdbool.h>
 #define LOG_LEVEL CONFIG_FLASH_LOG_LEVEL
 #include <zephyr/logging/log.h>
 #include <string.h>
@@ -75,10 +76,29 @@ void bgo_callback(flash_callback_args_t *p_args)
 	}
 }
 
-static bool flash_ra_valid_range(off_t area_size, off_t offset, size_t len)
+static bool flash_ra_valid_range(struct flash_hp_ra_data *flash_data, off_t offset, size_t len)
 {
-	if ((offset < 0) || (offset >= area_size) || ((area_size - offset) < len)) {
-		return false;
+	if (flash_data->FlashRegion == DATA_FLASH) {
+		if ((offset < 0) || (offset >= flash_data->area_size) ||
+		    (flash_data->area_size < len + offset)) {
+			return false;
+		}
+	} else {
+#if defined(CONFIG_DUAL_BANK_MODE)
+		if ((offset < 0) || (offset >= FLASH_HP_CF_DUAL_HIGH_END_ADDRESS) ||
+		    (offset >= FLASH_HP_CF_DUAL_LOW_END_ADDRESS &&
+		     offset < FLASH_HP_BANK2_OFFSET) ||
+		    ((len + offset) > FLASH_HP_CF_DUAL_HIGH_END_ADDRESS) ||
+		    ((len + offset) > FLASH_HP_CF_DUAL_LOW_END_ADDRESS &&
+		     (len + offset) < FLASH_HP_BANK2_OFFSET)) {
+			return false;
+		}
+#else
+		if ((offset < 0) || (offset >= flash_data->area_size) ||
+		    (flash_data->area_size < len + offset)) {
+			return false;
+		}
+#endif
 	}
 
 	return true;
@@ -88,7 +108,7 @@ static int flash_ra_read(const struct device *dev, off_t offset, void *data, siz
 {
 	struct flash_hp_ra_data *flash_data = dev->data;
 
-	if (!flash_ra_valid_range(flash_data->area_size, offset, len)) {
+	if (!flash_ra_valid_range(flash_data, offset, len)) {
 		return -EINVAL;
 	}
 
@@ -108,11 +128,13 @@ static int flash_ra_erase(const struct device *dev, off_t offset, size_t len)
 	struct flash_hp_ra_data *flash_data = dev->data;
 	struct flash_hp_ra_controller *dev_ctrl = flash_data->controller;
 	static struct flash_pages_info page_info_off, page_info_len;
-	fsp_err_t err = FSP_ERR_ASSERTION;
+	fsp_err_t err;
 	uint32_t block_num;
 	int rc, rc2;
+	int key = 0;
+	bool is_contain_end_block = false;
 
-	if (!flash_ra_valid_range(flash_data->area_size, offset, len)) {
+	if (!flash_ra_valid_range(flash_data, offset, len)) {
 		return -EINVAL;
 	}
 
@@ -132,35 +154,54 @@ static int flash_ra_erase(const struct device *dev, off_t offset, size_t len)
 		return -EINVAL;
 	}
 
-	rc2 = flash_get_page_info_by_offs(dev, (offset + len), &page_info_len);
-	if (rc2 != 0) {
-		return -EINVAL;
-	}
-
+	if (flash_data->FlashRegion == CODE_FLASH) {
 #if defined(CONFIG_DUAL_BANK_MODE)
-	/* Invalid offset in dual bank mode, this is reversed area. */
-	if ((page_info_off.index > FLASH_HP_CF_BLOCK_32KB_DUAL_LOW_END &&
-	     page_info_off.index < FLASH_HP_CF_BLOCK_8KB_HIGH_START) ||
-	    (page_info_len.index > FLASH_HP_CF_BLOCK_32KB_DUAL_LOW_END &&
-	     page_info_len.index < FLASH_HP_CF_BLOCK_8KB_HIGH_START)) {
-		return -EIO;
-	}
+		if ((offset + len) == (uint32_t)FLASH_HP_CF_DUAL_HIGH_END_ADDRESS) {
+			page_info_len.index = FLASH_HP_CF_BLOCK_32KB_DUAL_HIGH_END + 1;
+			is_contain_end_block = true;
+		}
+#else
+		if ((offset + len) == (uint32_t)DT_REG_SIZE(DT_NODELABEL(flash0))) {
+			page_info_len.index = FLASH_HP_CF_BLOCK_32KB_LINEAR_END + 1;
+			is_contain_end_block = true;
+		}
 #endif
+	} else {
+		if ((offset + len) == (uint32_t)DT_REG_SIZE(DT_NODELABEL(flash1))) {
+			page_info_len.index = FLASH_HP_DF_BLOCK_END + 1;
+			is_contain_end_block = true;
+		}
+	}
 
-	if ((offset + len) != (page_info_len.start_offset)) {
-		return -EIO;
+	if (!is_contain_end_block) {
+		rc2 = flash_get_page_info_by_offs(dev, (offset + len), &page_info_len);
+		if (rc2 != 0) {
+			return -EINVAL;
+		}
+		if ((offset + len) != (page_info_len.start_offset)) {
+			return -EIO;
+		}
 	}
 
 	block_num = (uint32_t)((page_info_len.index) - page_info_off.index);
 
 	if (block_num > 0) {
-		k_sem_take(&dev_ctrl->ctrl_sem, K_FOREVER);
+		if (flash_data->FlashRegion == CODE_FLASH) {
+			/* Disable interrupts during code flash operations */
+			key = irq_lock();
+		} else {
+			k_sem_take(&dev_ctrl->ctrl_sem, K_FOREVER);
+		}
 
 		err = R_FLASH_HP_Erase(&dev_ctrl->flash_ctrl,
 				       (long)(flash_data->area_address + offset), block_num);
 
 		if (err != FSP_SUCCESS) {
-			k_sem_give(&dev_ctrl->ctrl_sem);
+			if (flash_data->FlashRegion == CODE_FLASH) {
+				irq_unlock(key);
+			} else {
+				k_sem_give(&dev_ctrl->ctrl_sem);
+			}
 			return -EIO;
 		}
 
@@ -174,7 +215,11 @@ static int flash_ra_erase(const struct device *dev, off_t offset, size_t len)
 			}
 		}
 
-		k_sem_give(&dev_ctrl->ctrl_sem);
+		if (flash_data->FlashRegion == CODE_FLASH) {
+			irq_unlock(key);
+		} else {
+			k_sem_give(&dev_ctrl->ctrl_sem);
+		}
 	}
 
 	return 0;
@@ -185,8 +230,9 @@ static int flash_ra_write(const struct device *dev, off_t offset, const void *da
 	fsp_err_t err = FSP_ERR_ASSERTION;
 	struct flash_hp_ra_data *flash_data = dev->data;
 	struct flash_hp_ra_controller *dev_ctrl = flash_data->controller;
+	int key = 0;
 
-	if (!flash_ra_valid_range(flash_data->area_size, offset, len)) {
+	if (!flash_ra_valid_range(flash_data, offset, len)) {
 		return -EINVAL;
 	}
 
@@ -196,13 +242,22 @@ static int flash_ra_write(const struct device *dev, off_t offset, const void *da
 
 	LOG_DBG("flash: write 0x%lx, len: %u", (long)(offset + flash_data->area_address), len);
 
-	k_sem_take(&dev_ctrl->ctrl_sem, K_FOREVER);
+	if (flash_data->FlashRegion == CODE_FLASH) {
+		/* Disable interrupts during code flash operations */
+		key = irq_lock();
+	} else {
+		k_sem_take(&dev_ctrl->ctrl_sem, K_FOREVER);
+	}
 
 	err = R_FLASH_HP_Write(&dev_ctrl->flash_ctrl, (uint32_t)data,
 			       (long)(offset + flash_data->area_address), len);
 
 	if (err != FSP_SUCCESS) {
-		k_sem_give(&dev_ctrl->ctrl_sem);
+		if (flash_data->FlashRegion == CODE_FLASH) {
+			irq_unlock(key);
+		} else {
+			k_sem_give(&dev_ctrl->ctrl_sem);
+		}
 		return -EIO;
 	}
 
@@ -216,7 +271,11 @@ static int flash_ra_write(const struct device *dev, off_t offset, const void *da
 		}
 	}
 
-	k_sem_give(&dev_ctrl->ctrl_sem);
+	if (flash_data->FlashRegion == CODE_FLASH) {
+		irq_unlock(key);
+	} else {
+		k_sem_give(&dev_ctrl->ctrl_sem);
+	}
 
 	return 0;
 }

renesas/zephyr

drivers/flash/flash_hp_ra.c
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <stdbool.h>
 #define LOG_LEVEL CONFIG_FLASH_LOG_LEVEL
 #include <zephyr/logging/log.h>
 #include <string.h>
@@ -75,10 +76,29 @@ void bgo_callback(flash_callback_args_t *p_args)
 	}
 }
 
-static bool flash_ra_valid_range(off_t area_size, off_t offset, size_t len)
+static bool flash_ra_valid_range(struct flash_hp_ra_data *flash_data, off_t offset, size_t len)
 {
-	if ((offset < 0) || (offset >= area_size) || ((area_size - offset) < len)) {
-		return false;
+	if (flash_data->FlashRegion == DATA_FLASH) {
+		if ((offset < 0) || (offset >= flash_data->area_size) ||
+		    (flash_data->area_size < len + offset)) {
+			return false;
+		}
+	} else {
+#if defined(CONFIG_DUAL_BANK_MODE)
+		if ((offset < 0) || (offset >= FLASH_HP_CF_DUAL_HIGH_END_ADDRESS) ||
+		    (offset >= FLASH_HP_CF_DUAL_LOW_END_ADDRESS &&
+		     offset < FLASH_HP_CF_DUAL_HIGH_START_ADDRESS) ||
+		    ((len + offset) > FLASH_HP_CF_DUAL_HIGH_END_ADDRESS) ||
+		    ((len + offset) > FLASH_HP_CF_DUAL_LOW_END_ADDRESS &&
+		     (len + offset) < FLASH_HP_CF_DUAL_HIGH_START_ADDRESS)) {
+			return false;
+		}
+#else
+		if ((offset < 0) || (offset >= flash_data->area_size) ||
+		    (flash_data->area_size < len + offset)) {
+			return false;
+		}
+#endif
 	}
 
 	return true;
@@ -88,7 +108,7 @@ static int flash_ra_read(const struct device *dev, off_t offset, void *data, siz
 {
 	struct flash_hp_ra_data *flash_data = dev->data;
 
-	if (!flash_ra_valid_range(flash_data->area_size, offset, len)) {
+	if (!flash_ra_valid_range(flash_data, offset, len)) {
 		return -EINVAL;
 	}
 
@@ -108,11 +128,13 @@ static int flash_ra_erase(const struct device *dev, off_t offset, size_t len)
 	struct flash_hp_ra_data *flash_data = dev->data;
 	struct flash_hp_ra_controller *dev_ctrl = flash_data->controller;
 	static struct flash_pages_info page_info_off, page_info_len;
-	fsp_err_t err = FSP_ERR_ASSERTION;
+	fsp_err_t err;
 	uint32_t block_num;
 	int rc, rc2;
+	int key = 0;
+	bool is_contain_end_block = false;
 
-	if (!flash_ra_valid_range(flash_data->area_size, offset, len)) {
+	if (!flash_ra_valid_range(flash_data, offset, len)) {
 		return -EINVAL;
 	}
 
@@ -132,35 +154,54 @@ static int flash_ra_erase(const struct device *dev, off_t offset, size_t len)
 		return -EINVAL;
 	}
 
-	rc2 = flash_get_page_info_by_offs(dev, (offset + len), &page_info_len);
-	if (rc2 != 0) {
-		return -EINVAL;
-	}
-
+	if (flash_data->FlashRegion == CODE_FLASH) {
 #if defined(CONFIG_DUAL_BANK_MODE)
-	/* Invalid offset in dual bank mode, this is reversed area. */
-	if ((page_info_off.index > FLASH_HP_CF_BLOCK_32KB_DUAL_LOW_END &&
-	     page_info_off.index < FLASH_HP_CF_BLOCK_8KB_HIGH_START) ||
-	    (page_info_len.index > FLASH_HP_CF_BLOCK_32KB_DUAL_LOW_END &&
-	     page_info_len.index < FLASH_HP_CF_BLOCK_8KB_HIGH_START)) {
-		return -EIO;
-	}
+		if ((offset + len) == (uint32_t)FLASH_HP_CF_DUAL_HIGH_END_ADDRESS) {
+			page_info_len.index = FLASH_HP_CF_BLOCK_32KB_DUAL_HIGH_END + 1;
+			is_contain_end_block = true;
+		}
+#else
+		if ((offset + len) == (uint32_t)DT_REG_SIZE(DT_NODELABEL(flash0))) {
+			page_info_len.index = FLASH_HP_CF_BLOCK_32KB_LINEAR_END + 1;
+			is_contain_end_block = true;
+		}
 #endif
+	} else {
+		if ((offset + len) == (uint32_t)DT_REG_SIZE(DT_NODELABEL(flash1))) {
+			page_info_len.index = FLASH_HP_DF_BLOCK_END + 1;
+			is_contain_end_block = true;
+		}
+	}
 
-	if ((offset + len) != (page_info_len.start_offset)) {
-		return -EIO;
+	if (!is_contain_end_block) {
+		rc2 = flash_get_page_info_by_offs(dev, (offset + len), &page_info_len);
+		if (rc2 != 0) {
+			return -EINVAL;
+		}
+		if ((offset + len) != (page_info_len.start_offset)) {
+			return -EIO;
+		}
 	}
 
 	block_num = (uint32_t)((page_info_len.index) - page_info_off.index);
 
 	if (block_num > 0) {
-		k_sem_take(&dev_ctrl->ctrl_sem, K_FOREVER);
+		if (flash_data->FlashRegion == CODE_FLASH) {
+			/* Disable interrupts during code flash operations */
+			key = irq_lock();
+		} else {
+			k_sem_take(&dev_ctrl->ctrl_sem, K_FOREVER);
+		}
 
 		err = R_FLASH_HP_Erase(&dev_ctrl->flash_ctrl,
 				       (long)(flash_data->area_address + offset), block_num);
 
 		if (err != FSP_SUCCESS) {
-			k_sem_give(&dev_ctrl->ctrl_sem);
+			if (flash_data->FlashRegion == CODE_FLASH) {
+				irq_unlock(key);
+			} else {
+				k_sem_give(&dev_ctrl->ctrl_sem);
+			}
 			return -EIO;
 		}
 
@@ -174,7 +215,11 @@ static int flash_ra_erase(const struct device *dev, off_t offset, size_t len)
 			}
 		}
 
-		k_sem_give(&dev_ctrl->ctrl_sem);
+		if (flash_data->FlashRegion == CODE_FLASH) {
+			irq_unlock(key);
+		} else {
+			k_sem_give(&dev_ctrl->ctrl_sem);
+		}
 	}
 
 	return 0;
@@ -185,8 +230,9 @@ static int flash_ra_write(const struct device *dev, off_t offset, const void *da
 	fsp_err_t err = FSP_ERR_ASSERTION;
 	struct flash_hp_ra_data *flash_data = dev->data;
 	struct flash_hp_ra_controller *dev_ctrl = flash_data->controller;
+	int key = 0;
 
-	if (!flash_ra_valid_range(flash_data->area_size, offset, len)) {
+	if (!flash_ra_valid_range(flash_data, offset, len)) {
 		return -EINVAL;
 	}
 
@@ -196,13 +242,22 @@ static int flash_ra_write(const struct device *dev, off_t offset, const void *da
 
 	LOG_DBG("flash: write 0x%lx, len: %u", (long)(offset + flash_data->area_address), len);
 
-	k_sem_take(&dev_ctrl->ctrl_sem, K_FOREVER);
+	if (flash_data->FlashRegion == CODE_FLASH) {
+		/* Disable interrupts during code flash operations */
+		key = irq_lock();
+	} else {
+		k_sem_take(&dev_ctrl->ctrl_sem, K_FOREVER);
+	}
 
 	err = R_FLASH_HP_Write(&dev_ctrl->flash_ctrl, (uint32_t)data,
 			       (long)(offset + flash_data->area_address), len);
 
 	if (err != FSP_SUCCESS) {
-		k_sem_give(&dev_ctrl->ctrl_sem);
+		if (flash_data->FlashRegion == CODE_FLASH) {
+			irq_unlock(key);
+		} else {
+			k_sem_give(&dev_ctrl->ctrl_sem);
+		}
 		return -EIO;
 	}
 
@@ -216,7 +271,11 @@ static int flash_ra_write(const struct device *dev, off_t offset, const void *da
 		}
 	}
 
-	k_sem_give(&dev_ctrl->ctrl_sem);
+	if (flash_data->FlashRegion == CODE_FLASH) {
+		irq_unlock(key);
+	} else {
+		k_sem_give(&dev_ctrl->ctrl_sem);
+	}
 
 	return 0;
 }