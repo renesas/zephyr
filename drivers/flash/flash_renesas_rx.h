ab26999e7acdedd9b40c91346cf02265ffea4829
Quy Tran <quy.tran.pz@renesas.com>
2024-12-24 10:19:15 +0700

    drivers: flash: Add flash driver support for RX with flash type 1
    - Add support for flash driver on RX with flash type 1
    - Add bindings for flash driver on RX
    
    Signed-off-by: Tran Van Quy <quy.tran.pz@renesas.com>

---
+++ drivers/flash/flash_renesas_rx.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2025 Renesas Electronics Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_DRIVERS_FLASH_RENESAS_RX_H_
+#define ZEPHYR_DRIVERS_FLASH_RENESAS_RX_H_
+
+#include <zephyr/drivers/flash.h>
+
+enum flash_region {
+	CODE_FLASH,
+	DATA_FLASH,
+};
+
+struct flash_rx_config {
+	struct flash_parameters flash_rx_parameters;
+	bool bgo_enabled;
+};
+
+struct flash_rx_event {
+	volatile bool erase_complete;
+	volatile bool write_complete;
+	volatile bool error;
+};
+
+struct flash_rx_data {
+	struct flash_rx_controller *controller;
+	struct flash_rx_event flash_event;
+	enum flash_region FlashRegion;
+	uint32_t area_address;
+	uint32_t area_size;
+};
+
+#endif /* ZEPHYR_DRIVERS_FLASH_RENESAS_RX_H_ */
