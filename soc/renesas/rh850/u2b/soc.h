acb499a4750cbbce1bcf8541006a939bfeefbfc4
Phuc Le <phuc.le.ra@renesas.com>
2025-12-11 17:24:06 +0700

    soc:renesas:rh850: Add renesas RH850 soc
    Adds Kconfig, source file, linker scripts
    and CMakeLists for RH850/U2B device
    
    Signed-off-by: Phuc Le <phuc.le.ra@renesas.com>

---
+++ soc/renesas/rh850/u2b/soc.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2025 Renesas Electronics Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_SOC_RENESAS_RH850_SOC_H_
+#define ZEPHYR_SOC_RENESAS_RH850_SOC_H_
+
+#include <bsp_api.h>
+
+void soc_early_init_hook(void);
+
+#endif /* ZEPHYR_SOC_RENESAS_RH850_SOC_H_ */
