8f01a4c0a9e1f8dfce39238524cc31c6b66caacc
Phuc Le <phuc.le.ra@renesas.com>
2026-01-28 16:59:30 +0700

    boards: support RFP for RH850
    - Support Renesas Flash Programmer for RH850
    
    Signed-off-by: Phuc Le <phuc.le.ra@renesas.com>

--- boards/renesas/pb_rh850u2b_468pin/board.cmake
+++ boards/renesas/pb_rh850u2b_468pin/board.cmake
@@ -1,6 +1,10 @@
 # Copyright (c) 2026 Renesas Electronics Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(rfp "-device U2x" "-tool=E2" "-interface=CSI" "-erase")
+board_runner_args(rfp "--device=RH850/U2x" 
+    "--tool=e2" 
+    "--interface=csi" 
+    "--erase"
+    )
 
 include(${ZEPHYR_BASE}/boards/common/rfp.board.cmake)
