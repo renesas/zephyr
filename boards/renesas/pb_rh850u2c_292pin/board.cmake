7411a65740fc727019b81b230200f7017b13b3e6
Phuc Le <phuc.le.ra@renesas.com>
2026-04-02 11:39:20 +0700

    boards: renesas: support rh850u2c boards
    - Add rh850u2c Kconfig
    - Add rh850u2c device tree
    - Add rh850u2c documentation
    
    Signed-off-by: Phuc Le <phuc.le.ra@renesas.com>

--- boards/renesas/pb_rh850u2c_292pin/board.cmake
+++ boards/renesas/pb_rh850u2c_292pin/board.cmake
@@ -1,9 +1,9 @@
 # Copyright (c) 2026 Renesas Electronics Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(rfp "--device=RH850/U2x" 
-    "--tool=e2" 
-    "--interface=csi" 
+board_runner_args(rfp "--device=RH850/U2x"
+    "--tool=e2"
+    "--interface=csi"
     "--erase"
     )
 
