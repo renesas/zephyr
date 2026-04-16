333c2acb020dc88fd2741eff9714442495b8b341
Phi Tran <phi.tran.jg@bp.renesas.com>
2026-04-16 15:04:18 +0700

    drivers: ethernet: phy: renesas_ct25205: Add workqueue for monitor phy status
    Add workqueue for monitor phy status.
    
    Signed-off-by: Phi Tran <phi.tran.jg@bp.renesas.com>

--- drivers/ethernet/phy/phy_renesas_ct25205.c
+++ drivers/ethernet/phy/phy_renesas_ct25205.c
@@ -16,12 +16,12 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(LOG_MODULE_NAME);
 
-#define ETH_PHY_CT25205_REG_PLCACTRL0        (0x3FCA01U)
-#define ETH_PHY_CT25205_REG_PLCACTRL1        (0x3FCA02U)
-#define ETH_PHY_CT25205_REG_PLCAST           (0x3FCA03U)
-#define ETH_PHY_CT25205_REG_PLCAIDVER        (0x3FCA00U)
-#define ETH_PHY_CT25205_REG_PLCATOTMR        (0x3FCA04U)
-#define ETH_PHY_CT25205_REG_PLCABURST        (0x3FCA05U)
+#define ETH_PHY_CT25205_REG_PLCACTRL0 (0x3FCA01U)
+#define ETH_PHY_CT25205_REG_PLCACTRL1 (0x3FCA02U)
+#define ETH_PHY_CT25205_REG_PLCAST    (0x3FCA03U)
+#define ETH_PHY_CT25205_REG_PLCAIDVER (0x3FCA00U)
+#define ETH_PHY_CT25205_REG_PLCATOTMR (0x3FCA04U)
+#define ETH_PHY_CT25205_REG_PLCABURST (0x3FCA05U)
 
 struct renesas_ct25205_config {
 	const ether_phy_cfg_t *p_cfg;
@@ -29,10 +29,11 @@ struct renesas_ct25205_config {
 
 struct renesas_ct25205_data {
 	const struct device *dev;
-	ether_phy_instance_ctrl_t * p_ctrl;
+	ether_phy_instance_ctrl_t *p_ctrl;
 	struct phy_link_state state;
 	phy_callback_t cb;
 	void *cb_data;
+	struct k_work_delayable phy_monitor_work;
 };
 
 static int phy_renesas_ct25205_get_link(const struct device *dev, struct phy_link_state *state)
@@ -42,12 +43,9 @@ static int phy_renesas_ct25205_get_link(const struct device *dev, struct phy_lin
 
 	fsp_err = R_ETHER_PHY_LinkStatusGet(data->p_ctrl);
 
-	if(FSP_SUCCESS != fsp_err)
-	{
+	if (FSP_SUCCESS != fsp_err) {
 		state->is_up = false;
-	}
-	else
-	{
+	} else {
 		state->is_up = true;
 	}
 
@@ -56,7 +54,8 @@ static int phy_renesas_ct25205_get_link(const struct device *dev, struct phy_lin
 	return 0;
 }
 
-static int phy_renesas_ct25205_link_cb_set(const struct device *dev, phy_callback_t cb, void *user_data)
+static int phy_renesas_ct25205_link_cb_set(const struct device *dev, phy_callback_t cb,
+					   void *user_data)
 {
 	struct renesas_ct25205_data *data = dev->data;
 	struct phy_link_state state;
@@ -73,17 +72,37 @@ static int phy_renesas_ct25205_link_cb_set(const struct device *dev, phy_callbac
 	return 0;
 }
 
+static void phy_monitor_work_handler(struct k_work *work)
+{
+	struct k_work_delayable *dwork = k_work_delayable_from_work(work);
+	struct renesas_ct25205_data *const data =
+		CONTAINER_OF(dwork, struct renesas_ct25205_data, phy_monitor_work);
+	const struct device *dev = data->dev;
+
+	if (!data->cb) {
+		k_work_reschedule(&data->phy_monitor_work, K_MSEC(CONFIG_PHY_MONITOR_PERIOD));
+		return;
+	}
+
+	phy_renesas_ct25205_get_link(dev, &data->state);
+
+	data->cb(dev, &data->state, data->cb_data);
+
+	/* Submit delayed work */
+	k_work_reschedule(&data->phy_monitor_work, K_MSEC(CONFIG_PHY_MONITOR_PERIOD));
+}
+
 static int phy_renesas_ct25205_set_plca_cfg(const struct device *dev, struct phy_plca_cfg *plca_cfg)
 {
 	struct renesas_ct25205_data *data = dev->data;
 	uint32_t reg_val;
 
-	reg_val = (plca_cfg->node_count << 8U)|(plca_cfg->node_id);
+	reg_val = (plca_cfg->node_count << 8U) | (plca_cfg->node_id);
 	R_ETHER_PHY_Write(data->p_ctrl, ETH_PHY_CT25205_REG_PLCACTRL1, reg_val);
 
 	R_ETHER_PHY_Write(data->p_ctrl, ETH_PHY_CT25205_REG_PLCATOTMR, plca_cfg->to_timer);
 
-	reg_val = (plca_cfg->burst_count << 8U)|(plca_cfg->burst_timer);
+	reg_val = (plca_cfg->burst_count << 8U) | (plca_cfg->burst_timer);
 	R_ETHER_PHY_Write(data->p_ctrl, ETH_PHY_CT25205_REG_PLCABURST, reg_val);
 
 	R_ETHER_PHY_Read(data->p_ctrl, ETH_PHY_CT25205_REG_PLCACTRL0, &reg_val);
@@ -99,25 +118,25 @@ static int phy_renesas_ct25205_get_plca_cfg(const struct device *dev, struct phy
 	uint32_t reg_val;
 
 	R_ETHER_PHY_Read(data->p_ctrl, ETH_PHY_CT25205_REG_PLCAIDVER, &reg_val);
-	plca_cfg->version = (uint8_t) reg_val;
+	plca_cfg->version = (uint8_t)reg_val;
 
 	R_ETHER_PHY_Read(data->p_ctrl, ETH_PHY_CT25205_REG_PLCACTRL0, &reg_val);
-	plca_cfg->enable = (bool) (reg_val >> 15U);
+	plca_cfg->enable = (bool)(reg_val >> 15U);
 
 	R_ETHER_PHY_Read(data->p_ctrl, ETH_PHY_CT25205_REG_PLCACTRL1, &reg_val);
-	plca_cfg->node_id = (uint8_t) reg_val;
+	plca_cfg->node_id = (uint8_t)reg_val;
 
 	R_ETHER_PHY_Read(data->p_ctrl, ETH_PHY_CT25205_REG_PLCACTRL1, &reg_val);
-	plca_cfg->node_count = (uint8_t) (reg_val >> 8U);
+	plca_cfg->node_count = (uint8_t)(reg_val >> 8U);
 
 	R_ETHER_PHY_Read(data->p_ctrl, ETH_PHY_CT25205_REG_PLCABURST, &reg_val);
-	plca_cfg->burst_count = (uint8_t) (reg_val >> 8U);
+	plca_cfg->burst_count = (uint8_t)(reg_val >> 8U);
 
 	R_ETHER_PHY_Read(data->p_ctrl, ETH_PHY_CT25205_REG_PLCABURST, &reg_val);
-	plca_cfg->burst_timer = (uint8_t) reg_val;
+	plca_cfg->burst_timer = (uint8_t)reg_val;
 
 	R_ETHER_PHY_Read(data->p_ctrl, ETH_PHY_CT25205_REG_PLCATOTMR, &reg_val);
-	plca_cfg->to_timer = (uint8_t) reg_val;
+	plca_cfg->to_timer = (uint8_t)reg_val;
 
 	return 0;
 }
@@ -138,13 +157,13 @@ static int phy_renesas_ct25205_init(const struct device *dev)
 {
 	const struct renesas_ct25205_config *config = dev->config;
 	struct renesas_ct25205_data *data = dev->data;
+	data->dev = dev;
 	int err = 0;
 	fsp_err_t fsp_err = FSP_SUCCESS;
 
 	fsp_err = R_ETHER_PHY_Open(data->p_ctrl, config->p_cfg);
 
-	if(FSP_SUCCESS != fsp_err)
-	{
+	if (FSP_SUCCESS != fsp_err) {
 		LOG_ERR("PHY CT25205 Failed to Open");
 		err = -EIO;
 		goto end;
@@ -152,29 +171,32 @@ static int phy_renesas_ct25205_init(const struct device *dev)
 
 	fsp_err = R_ETHER_PHY_ChipInit(data->p_ctrl, config->p_cfg);
 
-	if(FSP_SUCCESS != fsp_err)
-	{
+	if (FSP_SUCCESS != fsp_err) {
 		LOG_ERR("PHY CT25205 Failed to Initialize");
 		err = -EIO;
 	}
+
+	k_work_init_delayable(&data->phy_monitor_work, phy_monitor_work_handler);
+	phy_monitor_work_handler(&data->phy_monitor_work.work);
+	LOG_INF("PHY CT25205 Initialized");
 
 end:
 	return err;
 }
 
 static DEVICE_API(ethphy, renesas_ct25205_phy_api) = {
-	.get_link     = phy_renesas_ct25205_get_link,
-	.link_cb_set  = phy_renesas_ct25205_link_cb_set,
+	.get_link = phy_renesas_ct25205_get_link,
+	.link_cb_set = phy_renesas_ct25205_link_cb_set,
 	.set_plca_cfg = phy_renesas_ct25205_set_plca_cfg,
 	.get_plca_cfg = phy_renesas_ct25205_get_plca_cfg,
 	.get_plca_sts = phy_renesas_ct25205_get_plca_sts,
 };
 
-#define RENESAS_CT25205_PHY_CONFIG(idx)                                                    \
+#define RENESAS_CT25205_PHY_CONFIG(idx)                                                            \
 	ether_phy_instance_ctrl_t g_ether_phy##idx##_ctrl;                                         \
                                                                                                    \
 	const ether_phy_lsi_cfg_t g_ether_phy##idx##_phy_lsi_cfg_list = {                          \
-		.address = DT_INST_PROP(idx, phy_lsi_addr),                                                                     \
+		.address = DT_INST_PROP(idx, phy_lsi_addr),                                        \
 		.type = ETHER_PHY_LSI_TYPE_CT25205_RTL,                                            \
 	};                                                                                         \
                                                                                                    \
@@ -185,34 +207,29 @@ static DEVICE_API(ethphy, renesas_ct25205_phy_api) = {
 		.eth_reg = ETHER_PHY_ETH_REG_T1S,                                                  \
 	};                                                                                         \
                                                                                                    \
-	const ether_phy_cfg_t g_ether_phy##idx##_cfg = {                                       \
+	const ether_phy_cfg_t g_ether_phy##idx##_cfg = {                                           \
 		.channel = idx,                                                                    \
-		.phy_lsi_address = DT_INST_PROP(idx, phy_lsi_addr),                                          \
+		.phy_lsi_address = DT_INST_PROP(idx, phy_lsi_addr),                                \
 		.phy_reset_wait_time = 0x00020000,                                                 \
 		.mii_bit_access_wait_time = 8,                                                     \
 		.phy_lsi_type = ETHER_PHY_LSI_TYPE_CT25205_RTL,                                    \
 		.flow_control = ETHER_PHY_FLOW_CONTROL_DISABLE,                                    \
 		.p_context = NULL,                                                                 \
 		.p_extend = &g_ether_phy##idx##_extended_cfg,                                      \
-	};                                                                                     \
-                                                                                           \
+	};
 
-#define RENESAS_CT25205_PHY_INIT_DRIVER(idx)                        \
-	RENESAS_CT25205_PHY_CONFIG(idx)                                 \
-                                                                    \
-                                                                    \
-	static const struct renesas_ct25205_config renesas_ct25205_##idx##_config = {     \
+#define RENESAS_CT25205_PHY_INIT_DRIVER(idx)                                                       \
+	RENESAS_CT25205_PHY_CONFIG(idx)                                                            \
+                                                                                                   \
+	static const struct renesas_ct25205_config renesas_ct25205_##idx##_config = {              \
 		.p_cfg = &g_ether_phy##idx##_cfg,                                                  \
-	};                                                                      \
-                                                                            \
-	static struct renesas_ct25205_data renesas_ct25205_##idx##_data = {       \
-		.p_ctrl = &g_ether_phy##idx##_ctrl,                                   \
-	};                                                                      \
-	DEVICE_DT_INST_DEFINE(idx, &phy_renesas_ct25205_init, NULL,               \
-							&renesas_ct25205_##idx##_data,                    \
-							&renesas_ct25205_##idx##_config,                  \
-							POST_KERNEL,                                    \
-							CONFIG_PHY_INIT_PRIORITY,                       \
-							&renesas_ct25205_phy_api);
+	};                                                                                         \
+                                                                                                   \
+	static struct renesas_ct25205_data renesas_ct25205_##idx##_data = {                        \
+		.p_ctrl = &g_ether_phy##idx##_ctrl,                                                \
+	};                                                                                         \
+	DEVICE_DT_INST_DEFINE(idx, &phy_renesas_ct25205_init, NULL, &renesas_ct25205_##idx##_data, \
+			      &renesas_ct25205_##idx##_config, POST_KERNEL,                        \
+			      CONFIG_PHY_RENESAS_CT25205_INIT_PRIORITY, &renesas_ct25205_phy_api);
 
 DT_INST_FOREACH_STATUS_OKAY(RENESAS_CT25205_PHY_INIT_DRIVER);
