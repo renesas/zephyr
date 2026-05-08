ffa758a4374ced254ea85f02d103be651a90de2b
Phuc Le <phuc.le.ra@renesas.com>
2026-05-08 09:13:47 +0700

    samples: drivers: Support rh850 ostm alarm samples
    - Add board overlay file for testing rh850 ostm counter
    - Modify main.c of alarm samples for rh850 ostm counter
    
    Signed-off-by: Phuc Le <phuc.le.ra@renesas.com>
    Signed-off-by: Vinh Dao <vinh.dao.wr@renesas.com>

--- samples/drivers/counter/alarm/src/main.c
+++ samples/drivers/counter/alarm/src/main.c
@@ -94,6 +94,8 @@ struct counter_alarm_cfg alarm_cfg;
 #define SAMPLE_TIMER DT_ALIAS(counter)
 #elif defined(CONFIG_COUNTER_ITE_IT51XXX)
 #define SAMPLE_TIMER DT_NODELABEL(counter0)
+#elif defined(CONFIG_COUNTER_RENESAS_RH850_OSTM)
+#define SAMPLE_TIMER DT_INST(0, renesas_rh850_ostm_counter)
 #else
 #error Unable to find a counter device node in devicetree
 #endif
