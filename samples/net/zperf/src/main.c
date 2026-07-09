469f8fed56aebea15a262bd76b66a97b61ab9eca
Duy Nguyen <duy.nguyen.xa@renesas.com>
2026-07-09 10:16:46 +0000

    samples: net: zperf: add USB host CDC-ECM support for RA8M2
    Add a board overlay/conf for ek_ra8m2_r7ka8m2jflcac_cm85 that enables
    the USB host stack, hub, and CDC-ECM class in place of the on-board
    Ethernet MAC, and initialize/enable the USB host controller and bring
    
    Add a board overlay/conf for ek_ra8m2_r7ka8m2jflcac_cm85 that enables
    the USB host stack, hub, and CDC-ECM class in place of the on-board
    
    Add a board overlay/conf for ek_ra8m2_r7ka8m2jflcac_cm85 that enables
    the USB host stack, hub, and CDC-ECM class in place of the on-board
    
    Add a board overlay/conf for ek_ra8m2_r7ka8m2jflcac_cm85 that enables
    the USB host stack, hub, and CDC-ECM class in place of the on-board
    Ethernet MAC, and initialize/enable the USB host controller and bring
    up the resulting network interface from zperf's main.c. Bump
    NET_BUF_DATA_SIZE to 1536 to match Ethernet MTU-sized buffers.
    
    Signed-off-by: Duy Nguyen <duy.nguyen.xa@renesas.com>

--- samples/net/zperf/src/main.c
+++ samples/net/zperf/src/main.c
@@ -21,6 +21,75 @@ LOG_MODULE_REGISTER(zperf, CONFIG_NET_ZPERF_LOG_LEVEL);
 #include <sample_usbd.h>
 #endif
 
+#if defined(CONFIG_USB_HOST_STACK)
+#include <zephyr/usb/usbh.h>
+#include <zephyr/drivers/usb/uhc.h>
+#include <zephyr/net/net_mgmt.h>
+#include <zephyr/net/net_event.h>
+
+USBH_CONTROLLER_DEFINE(sample_uhs_ctx, DEVICE_DT_GET(DT_NODELABEL(zephyr_uhc0)));
+
+#if defined(CONFIG_NET_CONFIG_SETTINGS)
+static struct net_mgmt_event_callback usbh_iface_cb;
+
+static void usbh_iface_up_handler(struct net_mgmt_event_callback *cb,
+				  uint64_t mgmt_event, struct net_if *iface)
+{
+	if (mgmt_event != NET_EVENT_IF_UP) {
+		return;
+	}
+
+	LOG_INF("USB host interface up, configuring static IP");
+	(void)net_config_init_app(NULL, "USB host network");
+}
+#endif /* CONFIG_NET_CONFIG_SETTINGS */
+
+static int zperf_usbh_init(void)
+{
+	int ret;
+
+#if defined(CONFIG_NET_CONFIG_SETTINGS)
+	net_mgmt_init_event_callback(&usbh_iface_cb, usbh_iface_up_handler,
+				     NET_EVENT_IF_UP);
+	net_mgmt_add_event_callback(&usbh_iface_cb);
+#endif
+
+	ret = usbh_init(&sample_uhs_ctx);
+	if (ret) {
+		LOG_ERR("Failed to init USB host: %d", ret);
+		return ret;
+	}
+
+	ret = usbh_enable(&sample_uhs_ctx);
+	if (ret) {
+		LOG_ERR("Failed to enable USB host: %d", ret);
+		return ret;
+	}
+
+	ret = uhc_bus_reset(sample_uhs_ctx.dev);
+	if (ret) {
+		LOG_ERR("Failed to signal bus reset: %d", ret);
+		return ret;
+	}
+
+	ret = uhc_bus_resume(sample_uhs_ctx.dev);
+	if (ret) {
+		LOG_ERR("Failed to signal bus resume: %d", ret);
+		return ret;
+	}
+
+	ret = uhc_sof_enable(sample_uhs_ctx.dev);
+	if (ret) {
+		LOG_ERR("Failed to enable SoF: %d", ret);
+		return ret;
+	}
+
+	return 0;
+}
+#endif /* CONFIG_USB_HOST_STACK */
+
+SYS_INIT(zperf_usbh_init, APPLICATION, 94);
+
 int main(void)
 {
 #if defined(CONFIG_USB_DEVICE_STACK_NEXT)
